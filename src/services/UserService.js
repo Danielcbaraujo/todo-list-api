2af7d69c736d543b3a372410c3278b4fd57464d3
Danielcbaraujo <daniel.carlos.araujo2@gmail.com>
2026-07-30 17:26:28 -0300

    atualização  do login

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -32,8 +32,52 @@ async register(data) {
         }
     );
 
-    return token;
+    return {
+        token,
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+        },
+    };
 }
+
+async login(data) {
+    const { email, password } = data;
+
+    const user = await UserRepository.findByEmail(email);
+
+    if (!user) {
+        throw new Error("Email ou senha inválidos");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error("Email ou senha inválidos");
+    }
+
+    const token = jwt.sign(
+        {
+            id: user.id,
+            email: user.email
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "1d"
+        }
+    );
+
+    return {
+        token,
+        user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+        },
+    };
+}
+
 }
 
 export default UserService;
